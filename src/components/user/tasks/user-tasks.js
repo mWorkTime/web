083777203befdb2980b1030b659ee2299e5880e8
illia <shabelnyk.ilya@gmail.com>
2020-11-12 22:41:00 +0200

    feat(web: user-tasks): enhance component - add cards with tasks. Add styles for the cards.

--- src/components/user/tasks/user-tasks.js
+++ src/components/user/tasks/user-tasks.js
@@ -1,16 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Tasks from '../../layouts/user/tasks'
 import { useDispatch, useSelector } from 'react-redux'
 import UserHeader from '../user-header'
 import { fetchAllTasks, fetchAllDepartments } from '../../../actions'
 import { renderListEmployees } from './render-list-employees'
-import { Form } from 'antd'
+import { Form, Button } from 'antd'
+import { DownloadOutlined } from '@ant-design/icons'
 import FormChooseDepartment from './form-choose-department'
 import Loader from '../../loader/loader'
 
 const UserTasks = () => {
   const { sidebarUser: { active }, taskData: { loading, employees, role }, departmentData: { departments } } = useSelector(state => state)
   const dispatch = useDispatch()
+  const [visible, setVisible] = useState(false)
   const [form] = Form.useForm()
 
   useEffect(() => {
@@ -22,6 +24,10 @@ const UserTasks = () => {
       dispatch(fetchAllDepartments())
     }
   }, [dispatch, departments])
+
+  const handleVisible = () => {
+    setVisible(!visible)
+  }
 
   return (
     <Tasks>
@@ -44,23 +50,134 @@ const UserTasks = () => {
               : null
           }
           <div className="tasks--wrapper--content">
-            <div className="wrapper--content--ls">
-              {
-                !loading && employees && Array.isArray(employees) && employees.length > 0
-                  ? <>
+
+            {
+              !loading && employees && Array.isArray(employees) && employees.length > 0
+                ? <>
+                  <div className="wrapper--content--ls">
                     <div className="content--ls__title">
                       Выберите рабочего и добавьте ему задачу
                     </div>
                     <div className="content-ls--list">
                       {renderListEmployees(employees)}
                     </div>
-                  </>
-                  : null
-              }
+                  </div>
+                  <div className="wrapper--content--rs">
 
-            </div>
-            <div className="wrapper--content--rs">
-
+                  </div>
+                </>
+                : null
+            }
+            <div className="content--tasks">
+              <div className="content--tasks__title">
+                Ваши задачи
+              </div>
+              <div className="content--tasks--board">
+                <div className="tasks--board__card ">
+                  <div className="tasks--board__title">Задача #1</div>
+                  <div className="tasks--board__priority high">Высокий</div>
+                  <div className="tasks--board__date" onClick={handleVisible}>3 д.</div>
+                  <div className={`tasks--board__runtime ${visible ? 'active' : ''}`}>
+                    11/4/2020, 6:25:25 PM - 11/7/2020, 2:05:27 PM
+                  </div>
+                  <div className="tasks--board--info">
+                    <div className="tasks--board--ls">
+                      <div className="board--info__block__desc">
+                        <div className="board--info__created__by">От кого: Илья Шараевский</div>
+                        <div className="board--info__desc">Создайте, измените или увольте - работника. Выберите для них
+                          роль "Работник", "Временный управляющий", "Управляющий".
+                        </div>
+                      </div>
+                      <Button className="board--info__download" type='primary' shape='round'><DownloadOutlined />Скачать
+                        файлы </Button>
+                    </div>
+                    <div className="board--info__comment">Прочитать комментарии</div>
+                    <div className="tasks--board__buttons">
+                      <Button className='board--btn start' shape='round'>Начать выполнение</Button>
+                      <Button className='board--btn review' shape='round'>Отправить на проверку</Button>
+                      <Button className='board--btn finish' disabled shape='round'>Завершить задание</Button>
+                    </div>
+                  </div>
+                </div>
+                <div className="tasks--board__card ">
+                  <div className="tasks--board__title">Задача #1</div>
+                  <div className="tasks--board__priority high">Высокий</div>
+                  <div className="tasks--board__date" onClick={handleVisible}>3 д.</div>
+                  <div className={`tasks--board__runtime ${visible ? 'active' : ''}`}>
+                    11/4/2020, 6:25:25 PM - 11/7/2020, 2:05:27 PM
+                  </div>
+                  <div className="tasks--board--info">
+                    <div className="tasks--board--ls">
+                      <div className="board--info__block__desc">
+                        <div className="board--info__created__by">От кого: Илья Шараевский</div>
+                        <div className="board--info__desc">Создайте, измените или увольте - работника. Выберите для них
+                          роль "Работник", "Временный управляющий", "Управляющий".
+                        </div>
+                      </div>
+                      <Button className="board--info__download" type='primary' shape='round'><DownloadOutlined />Скачать
+                        файлы </Button>
+                    </div>
+                    <div className="board--info__comment">Прочитать комментарии</div>
+                    <div className="tasks--board__buttons">
+                      <Button className='board--btn start' shape='round'>Начать выполнение</Button>
+                      <Button className='board--btn review' shape='round'>Отправить на проверку</Button>
+                      <Button className='board--btn finish' disabled shape='round'>Завершить задание</Button>
+                    </div>
+                  </div>
+                </div>
+                <div className="tasks--board__card ">
+                  <div className="tasks--board__title">Задача #1</div>
+                  <div className="tasks--board__priority high">Высокий</div>
+                  <div className="tasks--board__date" onClick={handleVisible}>3 д.</div>
+                  <div className={`tasks--board__runtime ${visible ? 'active' : ''}`}>
+                    11/4/2020, 6:25:25 PM - 11/7/2020, 2:05:27 PM
+                  </div>
+                  <div className="tasks--board--info">
+                    <div className="tasks--board--ls">
+                      <div className="board--info__block__desc">
+                        <div className="board--info__created__by">От кого: Илья Шараевский</div>
+                        <div className="board--info__desc">Создайте, измените или увольте - работника. Выберите для них
+                          роль "Работник", "Временный управляющий", "Управляющий".
+                        </div>
+                      </div>
+                      <Button className="board--info__download" type='primary' shape='round'><DownloadOutlined />Скачать
+                        файлы </Button>
+                    </div>
+                    <div className="board--info__comment">Прочитать комментарии</div>
+                    <div className="tasks--board__buttons">
+                      <Button className='board--btn start' shape='round'>Начать выполнение</Button>
+                      <Button className='board--btn review' shape='round'>Отправить на проверку</Button>
+                      <Button className='board--btn finish' disabled shape='round'>Завершить задание</Button>
+                    </div>
+                  </div>
+                </div>
+                <div className="tasks--board__card ">
+                  <div className="tasks--board__title">Задача #1</div>
+                  <div className="tasks--board__priority high">Высокий</div>
+                  <div className="tasks--board__date" onClick={handleVisible}>3 д.</div>
+                  <div className={`tasks--board__runtime ${visible ? 'active' : ''}`}>
+                    11/4/2020, 6:25:25 PM - 11/7/2020, 2:05:27 PM
+                  </div>
+                  <div className="tasks--board--info">
+                    <div className="tasks--board--ls">
+                      <div className="board--info__block__desc">
+                        <div className="board--info__created__by">От кого: Илья Шараевский</div>
+                        <div className="board--info__desc">Создайте, измените или увольте - работника. Выберите для них
+                          роль "Работник", "Временный управляющий", "Управляющий".
+                        </div>
+                      </div>
+                      <Button className="board--info__download" type='primary' shape='round'><DownloadOutlined />Скачать
+                        файлы </Button>
+                    </div>
+                    <div className="board--info__comment">Прочитать комментарии</div>
+                    <div className="tasks--board__buttons">
+                      <Button className='board--btn start' shape='round'>Начать выполнение</Button>
+                      <Button className='board--btn review' shape='round'>Отправить на проверку</Button>
+                      <Button className='board--btn finish' disabled shape='round'>Завершить задание</Button>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
