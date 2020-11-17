276576ca090d09973383266a1462c219c754a47a
illia <shabelnyk.ilya@gmail.com>
2020-11-17 22:38:25 +0200

    feat(web: task): some changes in task-create and task.action.

--- src/actions/task.action.js
+++ src/actions/task.action.js
@@ -10,12 +10,13 @@ import { dictionaryRoles } from '../items'
 
 const getConvertingEmployees = (arr) => {
   return arr.employees.reduce((acc, employee) => {
-    acc.push({ name: `${employee.name} ${employee?.surname}`, email: employee.email,
+    acc.push({
+      name: `${employee.name} ${employee?.surname}`, email: employee.email,
       role: { name: employee.role.name, normalName: dictionaryRoles[employee.role.name] },
       id: employee._id, department: employee.department.name
     })
     return acc
-  },[])
+  }, [])
 }
 
 const fetchAllTasks = () => (dispatch) => {
@@ -27,10 +28,12 @@ const fetchAllTasks = () => (dispatch) => {
         convertEmployees = getConvertingEmployees(data)
       }
 
-      dispatch({ type: FETCH_ALL_TASKS_SUCCESS, employees: convertEmployees,
-        tasks: data?.tasks || [], role: data.role, name: data.name })
+      dispatch({
+        type: FETCH_ALL_TASKS_SUCCESS, employees: convertEmployees,
+        tasks: data?.tasks || [], role: data.role, name: data.name
+      })
     })
-    .catch((err) => dispatch({ type: FETCH_ALL_TASKS_FAILURE, error: getErrorMsg(err)}))
+    .catch((err) => dispatch({ type: FETCH_ALL_TASKS_FAILURE, error: getErrorMsg(err) }))
 }
 
 const fetchEmployeesByDepartment = (values) => (dispatch) => {
@@ -43,21 +46,21 @@ const fetchEmployeesByDepartment = (values) => (dispatch) => {
       }
       dispatch({ type: FETCH_EMPLOYEES_BY_DEPARTMENT_SUCCESS, employees: convertEmployees })
     })
-    .catch((err) => dispatch({ type: FETCH_EMPLOYEES_BY_DEPARTMENT_FAILURE, error: getErrorMsg(err)}) )
+    .catch((err) => dispatch({ type: FETCH_EMPLOYEES_BY_DEPARTMENT_FAILURE, error: getErrorMsg(err) }))
 }
 
 const fetchCreateTask = (fieldsValue) => () => (dispatch) => {
-    const rangeTimeValue = fieldsValue['runtime'];
-    const values = {
-      ...fieldsValue,
-      runtime: [
-        rangeTimeValue[0].format('DD.MM.YYYY HH:mm:ss'),
-        rangeTimeValue[1].format('DD.MM.YYYY HH:mm:ss'),
-      ]
-    };
+  const rangeTimeValue = fieldsValue['runtime']
+  const values = {
+    ...fieldsValue,
+    runtime: [
+      rangeTimeValue[0].format('DD.MM.YYYY HH:mm:ss'),
+      rangeTimeValue[1].format('DD.MM.YYYY HH:mm:ss')
+    ]
+  }
   createTask(values)
-    .then(({ data }) => dispatch({ type: FETCH_CREATE_TASK_SUCCESS }))
-    .catch((err) => dispatch({ type: FETCH_CREATE_TASK_FAILURE, error: getErrorMsg(err)}))
+    .then(({ data: { task } }) => dispatch({ type: FETCH_CREATE_TASK_SUCCESS, id: task }))
+    .catch((err) => dispatch({ type: FETCH_CREATE_TASK_FAILURE, error: getErrorMsg(err) }))
 }
 
 const showComment = (commentId) => (dispatch) => {
--- src/components/user/tasks/task-create.js
+++ src/components/user/tasks/task-create.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import UserModal from '../user-modal'
 import renderTaskForm from './task-form'
 import TaskFileUpload from './task-file-upload'
@@ -9,9 +9,15 @@ import { fetchCreateTask } from '../../../actions'
 
 const TaskCreate = () => {
   const [form] = Form.useForm()
-  const { taskData: { modalTask, userId, user, disable } } = useSelector(state => state)
+  const { taskData: { modalTask, userId, user, disable, clearForm } } = useSelector(state => state)
   const dispatch = useDispatch()
   const hideModal = () => dispatch({ type: HIDE_MODAL_TASK, payload: 'task' })
+
+  useEffect(() => {
+    if(clearForm) {
+      form.resetFields(['name', 'desc', 'runtime', 'priority'])
+    }
+  }, [form, clearForm])
 
   return (
     <UserModal
